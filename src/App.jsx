da26f3efd908360e115c3acee66f774cc4609fec
nishad88T <nishad88@gmail.com>
2025-11-08 14:11:05 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Header} from './components/Header';
+import { Header } from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -35,4 +35,5 @@ function App() {
 }
 
 export default App;
+
 
