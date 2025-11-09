aa7977c95391c2598f644b7d7f736e058edcceb1
nishad88T <nishad88@gmail.com>
2025-11-09 07:14:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import FeaturesPage from './pages/FeaturesPage';
@@ -17,7 +15,6 @@ function App() {
     <Router>
       <ScrollToTop />
       <div className="min-h-screen flex flex-col">
-        <Header />
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<HomePage />} />
@@ -30,7 +27,6 @@ function App() {
             <Route path="/cookies" element={<CookiePage />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
